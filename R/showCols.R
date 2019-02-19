db2f35908424fc66461da4fa752a1bd813a87407
JepsonNomad <cjohn@ucdavis.edu>
2019-02-19 15:38:02 -0800

    Updated showCols to plot colored boxes with overlain color name

--- R/showCols.R
+++ R/showCols.R
@@ -4,14 +4,21 @@
 #' @export
 # version 0.0.1
 
-# http://www.sthda.com/english/wiki/colors-in-r
-showCols <- function(cl=colors(), bg = "grey",
-                     cex = 0.75, rot = 30) {
+# adapted from http://www.sthda.com/english/wiki/colors-in-r
+showCols <- function(cl=colors(), bg = "transparent",
+                     cex = 3, rot = 20) {
   m <- ceiling(sqrt(n <-length(cl)))
   length(cl) <- m*m; cm <- matrix(cl, m)
   require("grid")
-  grid.newpage(); vp <- viewport(w = .92, h = .92)
+  grid.newpage(); vp <- viewport(w = .5, h = .5)
   grid.rect(gp=gpar(fill=bg))
-  grid.text(cm, x = col(cm)/m, y = rev(row(cm))/m, rot = rot,
-            vp=vp, gp=gpar(cex = cex, col = cm))
+  grid.points(x = col(cm)/m, y = rev(row(cm))/m,
+              pch = 22, 
+              vp=vp, gp=gpar(cex = cex, 
+                             col = cm, 
+                             fill = cm))
+  grid.text(cm,x = col(cm)/m, y = rev(row(cm))/m,
+            vp=vp, rot = rot)
 }
+
+# showCols(KellyCols)
