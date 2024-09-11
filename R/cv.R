f1313e81a093d9e523a040a13c253e37e5755eb8
Christian John <cjohn@ucdavis.edu>
2024-09-11 11:45:38 -0700

    added standard error and coefficient of variation fx

---
+++ R/cv.R
@@ -0,0 +1,12 @@
+#' Calculate coefficient of variation of a vector of numbers
+#'
+#' @return Coefficient of variation
+#' @export
+# version 0.0.1
+
+cv <- function(x,...){
+  sd(x,...)/mean(x,...)
+}
+
+# cv(c(1:5,NA))
+# cv(c(1:5,NA),na.rm=T)
---
+++ R/se.R
@@ -0,0 +1,11 @@
+#' Calculate standard error of a vector of numbers
+#'
+#' @return Standard error
+#' @export
+# version 0.0.1
+
+se <- function(x,...){
+  sd(x,...)/sqrt(length(x))
+}
+
+# se(1:5)
