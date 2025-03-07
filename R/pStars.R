26b1803b179ac37e25c8789bd34b0592e1530724
Christian John <cjohn@ucdavis.edu>
2025-03-07 12:00:41 -0800

    added a p star function

---
+++ R/pStars.R
@@ -0,0 +1,29 @@
+#' Make a vector of asterisk strings for indicating p values
+#'
+#' @return vector
+#' @export
+# version 0.0.1
+
+pStars = function(x, addSpace = FALSE){
+  if(addSpace){
+    ifelse(x > 0.1,
+           "",
+           ifelse(x > 0.05,
+                  " .",
+                  ifelse(x > 0.01,
+                         " *",
+                         ifelse(x > 0.001,
+                                " **",
+                                " ***"))))
+  }else{
+    ifelse(x > 0.1,
+           "",
+           ifelse(x > 0.05,
+                  ".",
+                  ifelse(x > 0.01,
+                         "*",
+                         ifelse(x > 0.001,
+                                "**",
+                                "***"))))
+  }
+}
